93dce1ea77e674354fccfdad5769ebad399c59b2
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2023-03-13 18:58:12 -0600

    Style shoegrid
    Corresponds to the forth exercise

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -14,6 +14,10 @@ const ShoeGrid = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 30px;
+`;
 
 export default ShoeGrid;
